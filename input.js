14dd3dd290c2c93a67864a21414316696649592e
Brittany Cowper <brittferren@gmail.com>
2023-03-22 13:18:09 -0500

    implemented sending canned messages to the server using 1, 2, 3, 5 number key strokes within handleUserInput function

--- input.js
+++ input.js
@@ -12,8 +12,8 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = (key) => {
+  // movement keys
   if (key === '\u0077') {
-    console.log("w pressed");
     connection.write("Move: up");
   } else if (key === '\u0061') {
     connection.write("Move: left");
@@ -21,6 +21,15 @@ const handleUserInput = (key) => {
     connection.write("Move: down");
   } else if (key === '\u0064') {
     connection.write("Move: right");
+  } else if (key === '\u0031') {
+    // banter keys
+    connection.write("Say: Hissssssss");
+  } else if (key === '\u0032') {
+    connection.write("Say: Catch me if you can");
+  } else if (key === '\u0033') {
+    connection.write("Say: I can nom all day");
+  } else if (key === '\u0035') {
+    connection.write("Say: I am sssspeed");
   } else if (key === '\u0003') {
     process.exit();
   }
